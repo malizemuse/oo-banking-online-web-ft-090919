7cd92af5242a26aeee2301152ba89830a5466fff
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-27 18:08:41 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,11 +16,11 @@ class BankAccount
   end 
   
   def display_balance 
-    "Your balance is $#{balance}."
+    "Your balance is $#{@balance}."
   end 
   
   def valid? 
-    true if 
+    
   end 
   
 end 
