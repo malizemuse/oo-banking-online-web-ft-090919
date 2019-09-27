eaf0596bcfbff4513a9714ac16d3aba20301504e
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-27 18:15:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   
   attr_accessor :sender, :receiver
   
-  def initialize(sender, receiver)
+  def initialize(sender, receiver, status="pending")
     @sender = sender
     @receiver = receiver
   end 
